bd874d40857a42d6702a4d23e86f29ee7d1dd736
AkuLinker <AkuLinker@gmail.com>
2022-05-24 14:41:53 +0300

    changes in labels of form for placeholders in html

--- yacut/forms.py
+++ yacut/forms.py
@@ -5,12 +5,12 @@ from wtforms.validators import URL, DataRequired, Length, Optional
 
 class URLForm(FlaskForm):
     original_link = URLField(
-        'Оригинальная ссылка',
+        'Длинная ссылка',
         validators=[DataRequired(message='Обязательное поле'),
                     URL(message='Ссылка некорректна')]
     )
     custom_id = StringField(
-        'Кастомная ссылка',
+        'Ваш вариант короткой ссылки',
         validators=[Length(1, 16), Optional()]
     )
     submit = SubmitField('Создать')
